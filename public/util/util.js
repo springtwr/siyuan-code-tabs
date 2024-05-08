3fda185be2d07d41a465113354a799a2f36b1492
springtwr <springtwr@outlook.com>
2024-05-08 11:10:38 +0800

    - 功能基本可用，但还无法自动跟随主题变换样式

---
+++ public/util/util.js
@@ -0,0 +1,72 @@
+function openTag(evt) {
+    let tabContainer = evt.target.parentNode.parentNode;
+    let tabItems = tabContainer.querySelectorAll('.tab-item');
+    let tabContents = tabContainer.querySelectorAll('.tab-content');
+    tabItems.forEach((tabItem, index) => {
+        if (tabItem === evt.target) {
+            tabItem.classList.add('tab-item--active');
+            tabContents[index].classList.add('tab-content--active');
+        } else {
+            tabItem.classList.remove('tab-item--active');
+            tabContents[index].classList.remove('tab-content--active');
+        }
+    });
+}
+
+function toggle(evt) {
+    const findRootParent = (element) => {
+        let parent = element;
+        while (parent.parentNode) {
+            parent = parent.parentNode;
+        }
+        return parent;
+    }
+    const shadowRoot = findRootParent(evt.target);
+    console.log(shadowRoot)
+    const nodeId = shadowRoot.querySelector('.tabs-container').id;
+    console.log(nodeId);
+    const codeText = shadowRoot.querySelector('.tab-sourcecode').textContent;
+    appendBlock("markdown", `\`\`\`tab\n${codeText}\`\`\``, nodeId).then(() => {
+        deleteBlock(nodeId).then();
+    })
+}
+
+async function appendBlock(dataType, data, parentID) {
+    let payload = {
+        dataType,
+        data,
+        parentID
+    }
+    let url = '/api/block/appendBlock';
+    return request(url, payload);
+}
+
+async function deleteBlock(id) {
+    let data = {
+        id: id
+    }
+    let url = '/api/block/deleteBlock';
+    return request(url, data);
+}
+
+async function request(urlSuffix, data) {
+    const baseUrl = 'http://127.0.0.1:6806';
+    const url = baseUrl + urlSuffix;
+    console.log(data);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        console.log(response);
+        if (response.code === 0) {
+            return await response;
+        }
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        throw error;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {Plugin} from "siyuan";
-import {deleteBlock, insertBlock, updateBlock} from "@/api";
+import {appendBlock, deleteBlock, updateBlock} from "@/api";
 import "@/index.scss";
-import hljs from 'highlight.js';
+import hljs from "highlight.js";
 
 export default class CodeTabs extends Plugin {
     private blockIconEventBindThis = this.blockIconEvent.bind(this);
@@ -26,116 +26,97 @@ export default class CodeTabs extends Plugin {
         });
     }
 
-    private convertToTabs(detail: any) {
-        detail.blockElements.forEach((item: HTMLElement) => {
+    private async convertToTabs(detail: any) {
+        for (const item of detail.blockElements) {
             const editElement = item.querySelector('[contenteditable="true"]');
             if (editElement && item.getAttribute("data-type") === "NodeCodeBlock") {
                 const codeText = editElement.textContent;
+                const id = item.dataset.nodeId;
+                const tempBlock = await this.getBackgroundColor(id);
+                const tempId = tempBlock["tempId"];
+                const codeBg = tempBlock["bg"];
+                console.log(window.getComputedStyle(editElement).backgroundColor);
+                console.log(codeBg);
                 // 创建思源笔记中的HTMLBlock
-                const htmlBlock = this.createHtmlBlock(item.dataset.nodeId, codeText);
+                const htmlBlock = this.createHtmlBlock(id, codeText, codeBg);
                 // 更新代码块
                 if (codeText.split("tab:").length > 1) {
                     this.update(htmlBlock, item.dataset.nodeId).then(() => {
                         console.log("更新代码块");
+                        deleteBlock(tempId);
                     });
                 }
             }
-        });
+        }
     }
 
     private async update(data: string, nodeId: string) {
         updateBlock("dom", data, nodeId).then(() => {
-            const node = document.documentElement
-                .getElementsByClassName("protyle-wysiwyg protyle-wysiwyg--attr")[0]
-                .querySelector(`[data-node-id="${nodeId}"]`);
-            const shadow = node.querySelector('[data-content]').shadowRoot;
-            // 高亮代码块
-            const codeContents = shadow.querySelectorAll('pre code');
-            codeContents.forEach((code: HTMLPreElement) => {
-                hljs.highlightElement(code);
-            });
-            const toggleSwitch = shadow.querySelector('.tab-toggle');
-            if (toggleSwitch) {
-                toggleSwitch.addEventListener('click', () => {
-                    // 从标签页切换回代码块
-                    this.toggleToCode(node);
-                });
-            } else {
-                console.error('Element not found.');
-            }
+            console.log("update done");
+            // const node = document.documentElement
+            //     .getElementsByClassName("protyle-wysiwyg protyle-wysiwyg--attr")[0]
+            //     .querySelector(`[data-node-id="${nodeId}"]`);
+            // const shadow = node.querySelector('[data-content]').shadowRoot;
+            // const toggleSwitch = shadow.querySelector('.tab-toggle');
+            // if (toggleSwitch) {
+            //     toggleSwitch.addEventListener('click', () => {
+            //         // 从标签页切换回代码块
+            //         this.toggleToCode(node);
+            //     });
+            // } else {
+            //     console.error('Element not found.');
+            // }
         });
     }
 
-    private toggleToCode(htmlBlock: Element) {
-        const codeText = htmlBlock.getElementsByTagName('protyle-html')[0]
-            .shadowRoot.querySelector('.tab-sourcecode').textContent;
-        const nodeId = htmlBlock.getAttribute("data-node-id");
-        // 先插入代码块，然后再删除原本的HTML块
-        insertBlock("markdown", `\`\`\`tab\n${codeText}\`\`\``, nodeId).then(() => {
-            deleteBlock(nodeId).then();
-        })
+    // private toggleToCode(htmlBlock: Element) {
+    //     const codeText = htmlBlock.getElementsByTagName('protyle-html')[0]
+    //         .shadowRoot.querySelector('.tab-sourcecode').textContent;
+    //     const nodeId = htmlBlock.getAttribute("data-node-id");
+    //     // 先插入代码块，然后再删除原本的HTML块
+    //     appendBlock("markdown", `\`\`\`tab\n${codeText}\`\`\``, nodeId).then(() => {
+    //         deleteBlock(nodeId).then();
+    //     })
+    // }
+
+    private createHtmlBlock(id: string, codeText: string, codeBg: string) {
+        const html_1 = `
+            <div data-node-id="${id}" data-type="NodeHTMLBlock" class="render-node" data-subtype="block">
+                <div class="protyle-icons">
+                    <span aria-label="编辑" class="b3-tooltips__nw b3-tooltips protyle-icon protyle-icon--first protyle-action__edit">
+                        <svg><use xlink:href="#iconEdit"></use></svg>
+                    </span>
+                    <span aria-label="更多" class="b3-tooltips__nw b3-tooltips protyle-icon protyle-action__menu protyle-icon--last">
+                        <svg><use xlink:href="#iconMore"></use></svg>
+                    </span>
+                </div>
+                <div>`.replace(/>\s+</g, '><').trim();
+        const html_2 = `<protyle-html data-content="${this.createProtyleHtml(id, codeText, codeBg)}"></protyle-html>`;
+        const html_3 = `<span style="position: absolute"></span>
+                </div>
+                <div class="protyle-attr" contenteditable="false"></div>
+            </div>`.replace(/>\s+</g, '><').trim();
+        return html_1 + html_2 + html_3;
     }
 
-    /**
-     * 创建思源文档中的HTMLBlock
-     * @param id 要转换的代码块的data-node-id
-     * @param codeText 要转换的代码块中的内容
-     * @return HTMLBlock的HTML字符串
-     * @private
-     */
-    private createHtmlBlock(id: string, codeText: string) {
-        return `
-<div data-node-id="${id}" 
-    data-type="NodeHTMLBlock" 
-    class="render-node" 
-    data-subtype="block">
-    <div class="protyle-icons">
-        <span aria-label="编辑" class="b3-tooltips__nw b3-tooltips protyle-icon protyle-icon--first protyle-action__edit">
-            <svg><use xlink:href="#iconEdit"></use></svg>
-        </span>
-        <span aria-label="更多" class="b3-tooltips__nw b3-tooltips protyle-icon protyle-action__menu protyle-icon--last">
-            <svg><use xlink:href="#iconMore"></use></svg>
-        </span>
-    </div>
-    <div>
-        <protyle-html data-content="${this.createProtyleHtml(codeText)}"></protyle-html>
-        <span style="position: absolute">​</span>
-    </div>
-    <div class="protyle-attr" contenteditable="false"></div>
-</div>`;
-    }
-
-    private createProtyleHtml(codeText: string) {
-        const tabContainer = this.createTabs(codeText);
-        const html = `  
-<div>  
-    <link rel="stylesheet" href="/stage/protyle/js/highlight.js/styles/idea.min.css">  
-    <link rel="stylesheet" href="/plugins/code-tabs/index.css">  
-    ${tabContainer}  
-    <script>  
-        function openTag(evt) {  
-            let tabContainer = evt.target.closest('.tab-container');  
-            let tabItems = tabContainer.querySelectorAll('.tab-item');  
-            let tabContents = tabContainer.querySelectorAll('.tab-content');  
-            tabItems.forEach((tabItem, index) => {  
-                if (tabItem === evt.target) {  
-                    tabItem.classList.add('tab-item--active');  
-                    tabContents[index].classList.add('tab-content--active');  
-                } else {  
-                    tabItem.classList.remove('tab-item--active');  
-                    tabContents[index].classList.remove('tab-content--active');  
-                }  
-            });  
-        }  
-    </script>  
-</div>  
-    `;
+    private createProtyleHtml(id: string, codeText: string, codeBg: string) {
+        const codeStyle = document.getElementById('protyleHljsStyle').getAttribute('href');
+        const html_1 = `  
+            <div> 
+                <link rel="stylesheet" href="${codeStyle}">  
+                <link rel="stylesheet" href="/plugins/code-tabs/index.css">`
+            .replace(/>\s+</g, '><').trim();
+        const html_2 = this.createTabs(id, codeText, codeBg);
+        const html_3 = `<script src="/plugins/code-tabs/util/util.js"></script>
+            </div>`.replace(/>\s+</g, '><').trim();
+        const html = html_1 + html_2 + html_3;
         return this.escapeHtml(html);
     }
 
-    private createTabs(codeText: string) {
+    private createTabs(id: string, codeText: string, codeBg: string) {
         const tabContainer = document.createElement('div');
         tabContainer.className = 'tabs-container';
+        tabContainer.id = id;
 
         // tabs 包含所有标签页的标题
         let tabs = document.createElement("div");
@@ -151,20 +132,6 @@ export default class CodeTabs extends Plugin {
             const language = codeBlock.shift()?.trim();
             const code = codeBlock.join('\n').trim();
 
-            // 先添加自定义内容
-            if (i === 1) {
-                // 切换键，用来将标签页切回代码块
-                let tabCustomTag = document.createElement("div");
-                tabCustomTag.className = "tab-toggle";
-                tabCustomTag.textContent = this.i18n.toggleToCode;
-                tabs.appendChild(tabCustomTag);
-                // 用来保存原始的代码块内容
-                let tabSourceCode = document.createElement('div');
-                tabSourceCode.className = "tab-sourcecode";
-                tabSourceCode.textContent = code;
-                tabContents.appendChild(tabSourceCode);
-            }
-
             // fill up tab
             let tab = document.createElement("div");
             tab.className = "tab-item";
@@ -177,38 +144,54 @@ export default class CodeTabs extends Plugin {
             let content = document.createElement('div');
             content.className = "tab-content";
             if (i === 1) content.classList.add("tab-content--active");
-
-            let codeElement = document.createElement('code');
-            codeElement.className = 'language-' + language;
-            codeElement.textContent = code.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-            let preElement = document.createElement('pre');
-            preElement.appendChild(codeElement);
-            content.appendChild(preElement);
+            const pre = document.createElement('pre');
+            pre.className = "hljs";
+            /* 不知道为什么，反正只有这样才能在思源中正确显示带内容的尖括号，如<stdio.h>*/
+            try {
+                pre.innerHTML = hljs.highlight(language,code, true).value
+                    .replace(/&lt;/g, '&amp;amp;lt;')
+                    .replace(/&gt;/g, '&amp;amp;gt;');
+            }catch (err){
+                pre.innerHTML = hljs.highlight("plaintext", code, true).value
+                    .replace(/&lt;/g, '&amp;amp;lt;')
+                    .replace(/&gt;/g, '&amp;amp;gt;');
+            }
+            pre.style.backgroundColor = codeBg;
+            content.appendChild(pre);
             tabContents.appendChild(content);
         }
+        // 最后添加自定义内容
+        // 切换键，用来将标签页切回代码块
+        let tabCustomTag = document.createElement("div");
+        tabCustomTag.className = "tab-toggle";
+        tabCustomTag.setAttribute('onclick', 'toggle(event)');
+        tabCustomTag.textContent = this.i18n.toggleToCode;
+        tabs.appendChild(tabCustomTag);
+        // 用来保存原始的代码块内容
+        let tabSourceCode = document.createElement('div');
+        tabSourceCode.className = "tab-sourcecode";
+        /* 不知道为什么，反正只有这样才能在思源中正确显示带内容的尖括号，如<stdio.h>*/
+        tabSourceCode.innerHTML = codeText.replace(/</g, '&amp;amp;lt;')
+            .replace(/>/g, '&amp;amp;gt;');
+        tabContents.appendChild(tabSourceCode);
+
         tabContainer.appendChild(tabs);
         tabContainer.appendChild(tabContents);
         return tabContainer.outerHTML;
     }
 
-    // private getCurrentTime() {
-    //     // 获取当前时间
-    //     const currentDate = new Date();
-    //     // 获取当前时间的年、月、日、小时、分钟和秒
-    //     const currentYear = currentDate.getFullYear();
-    //     const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    //     const currentDay = currentDate.getDate().toString().padStart(2, '0');
-    //     const currentHour = currentDate.getHours().toString().padStart(2, '0');
-    //     const currentMinute = currentDate.getMinutes().toString().padStart(2, '0');
-    //     const currentSecond = currentDate.getSeconds().toString().padStart(2, '0');
-    //     // 格式化当前时间
-    //     return `${currentYear}${currentMonth}${currentDay}${currentHour}${currentMinute}${currentSecond}`;
-    // }
+    private async getBackgroundColor(id: string) {
+        const result = await appendBlock("markdown", "\`\`\`python\nprint(\"error\")\n", id);
+        const tempId = result[0].doOperations[0].id;
+        const tempElement = document.querySelector(`[data-node-id="${tempId}"]`).querySelector('[contenteditable="true"]');
+        console.log(tempElement);
+        const bg = window.getComputedStyle(tempElement).backgroundColor;
+        // await deleteBlock(tempId);
+        return {tempId, bg};
+    }
 
     private escapeHtml(input: string): string {
         return input.replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
             .replace(/"/g, '&quot;')
             .replace(/'/g, '&#39;');
     }

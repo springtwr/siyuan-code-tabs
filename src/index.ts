d2f89963fdc6553858914907d1b5f7c0173db234
springtwr <springtwr@outlook.com>
2024-11-23 18:01:47 +0800

    - 将某些固定样式重新写回内联样式表，防止在某些情况下的浏览器显示异常

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,57 @@ export default class CodeTabs extends Plugin {
             <link rel="stylesheet" href="/plugins/code-tabs/asset/katex.min.css">
             <link rel="stylesheet" href="/plugins/code-tabs/asset/code-tabs.css">
             <link rel="stylesheet" href="/plugins/code-tabs/background.css">
+            <style>
+                .tabs-container {
+                    display: block;
+                    position: relative;
+                    will-change: background-color;
+                }
+                
+                .tabs-outer {
+                    display: flex;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    justify-content: space-between;
+                    align-items: center;
+                }
+                
+                .tabs {
+                    order: 0;
+                    display: flex;
+                    width: calc(100% - 6em);
+                    height: 100%;
+                    align-items: center;
+                    overflow-x: auto;
+                    overflow-y: hidden;
+                    scroll-behavior: smooth;
+                    -webkit-overflow-scrolling: touch;
+                    transform: translate3d(0, 0, 0); /* 启用硬件加速 */
+                    scrollbar-width: none;
+                    -ms-overflow-style: none;
+                }
+                
+                .tabs::-webkit-scrollbar {
+                    display: none;
+                }
+                
+                .tab-toggle {
+                    order: 1;
+                    width: 6em;
+                    height: 100%;
+                    text-align: center;
+                    font-weight: bold;
+                    padding: 5px;
+                }
+                
+                .tab-contents {
+                    word-break: break-word;
+                    font-variant-ligatures: none;
+                    position: relative;
+                }
+            </style>
             <div class="tabs-container">
                 <div class="tabs-outer">
                     <div class="tabs"></div>
